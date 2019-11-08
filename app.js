6389201ab971a9991d80ce6525eb9a0cc75ef3bf
Brent Groves <brent.groves@gmail.com>
2019-11-08 17:11:09 -0500

    Added record production

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 // START WITH NPM START
-
+// https://stackoverflow.com/questions/22820162/mustache-js-escaping
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mustacheExpress = require('mustache-express');
+
 var router = express.Router();
 //https://expressjs.com/en/starter/generator.html
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var chatRouter = require('./routes/chat');
+//var usersRouter = require('./routes/users');
+//var chatRouter = require('./routes/chat');
 
 var app = express();
 
+
+// Register '.mustache' extension with The Mustache Express
+app.engine('html', mustacheExpress());
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+
+
+
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'mustache');
+//app.set('views', __dirname + '/views');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,8 +40,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/chat',chatRouter);
+//app.use('/users', usersRouter);
+//app.use('/chat',chatRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
